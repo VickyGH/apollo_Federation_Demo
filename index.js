8f718ab2bc62113a92b067d2d417a3c4aa6926e0
Vicky Garcia <gar_vicky93@hotmail.com>
2021-11-11 17:50:12 -0600

    Partnercode y urls

--- index.js
+++ index.js
@@ -26,19 +26,20 @@ const gateway = new ApolloGateway({
     //{ name: 'gmm',    url: 'http://localhost:8101/graphql/' },
     //{ name: 'report',    url: 'http://localhost:8102/graphql/' },
     //{ name: 'mortgage',    url: 'http://localhost:8103/graphql/' },
-    //{ name: 'mortgage',    url: 'http://localhost:8104/graphql/' },
+    //{ name: 'package',    url: 'http://localhost:8104/graphql/' },
 
 
     { name: "access",  url: "https://dommy.v2-stag.ixuapistemp.ml/graphql/" },
     { name: "content", url: "https://content.v2-stag.ixuapis.com/graphql/" },
     { name: "payment", url: "https://payments-staging.v2-stag.ixuapistemp.ml/graphql/" },
-    { name: "website", url: "https://websites-service.staging.ixulabs.com/graphql/" },
+    //{ name: "website", url: "https://websites-service.staging.ixulabs.com/graphql/" },
     //{ name: "article",    url: "https://blog.staging.ixulabs.com/graphql/" },
     //{ name: "contests",url: "https://contests-service.staging.ixulabs.com/graphql/" },
     { name: "logs",    url: "https://logs-service.staging.ixulabs.com/graphql/" },
     { name: "chatbots",url: "https://chatbots-service.ixulabs.com/graphql" },
     { name: "appointment", url: "https://appointments-service.staging.ixulabs.com/graphql" },
-    //{ name: "cotizador-autos", url: "https://cotizador-autos-service.staging.ixulabs.com/graphql/" },
+    { name: "goal",    url: "https://goals-service.staging.ixulabs.com/graphql/" },
+    { name: "cotizador-autos", url: "https://life-service.staging.ixulabs.com/graphql/" }
   ],
    buildService({ name, url }) {
     return new AuthenticatedDataSource({ url });
